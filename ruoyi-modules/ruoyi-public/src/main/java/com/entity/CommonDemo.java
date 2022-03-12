40d3fd8dbfab5e15dc6dc91926603fbe77e62bce
oweson <570347720@qq.com>
2022-03-12 13:51:47 +0800

    新增lombok和hutools依赖！

--- ruoyi-modules/ruoyi-public/src/main/java/com/entity/CommonDemo.java
+++ ruoyi-modules/ruoyi-public/src/main/java/com/entity/CommonDemo.java
@@ -1,30 +1,14 @@
 package com.entity;
 
 import com.ruoyi.common.core.web.domain.BaseEntity;
+import lombok.Data;
 
 /**
  * @author oweson
  * @date 2022/3/12 11:33
  */
-
-
+@Data
 public class CommonDemo extends BaseEntity {
     private Long id;
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
